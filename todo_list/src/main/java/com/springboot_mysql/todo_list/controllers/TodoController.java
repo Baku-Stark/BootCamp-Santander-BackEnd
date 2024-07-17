f84be1735546b3215d0d089d7834282c424be086
Baku-Stark <wysthellyjessemata13@gmail.com>
2024-07-16 23:12:25 -0300

    update : `DIO Project`

--- todo_list/src/main/java/com/springboot_mysql/todo_list/controllers/TodoController.java
+++ todo_list/src/main/java/com/springboot_mysql/todo_list/controllers/TodoController.java
@@ -1,14 +1,28 @@
 package com.springboot_mysql.todo_list.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.springboot_mysql.todo_list.repository.TodoRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping
 public class TodoController {
+    TodoRepository todoRepository = new TodoRepository();
+
     @GetMapping("/get_all_todos")
-    public String index() {
-        return "Todo List";
+    public ArrayList<ArrayList<String>> index() {
+        return todoRepository.getAllTodos();
+    }
+
+    @GetMapping("/get_id_todo/{id}")
+    public ArrayList<String> get_id_todo(@PathVariable int id) {
+        return todoRepository.getTodoForID(id);
+    }
+
+    @PostMapping("/create_todo")
+    public ArrayList<String> createTodo(String title, String description) {
+        //System.out.println("Title: " + title + ", Description: " + description);
+        return todoRepository.createTodo(title, description);
     }
 }
---
+++ todo_list/src/main/java/com/springboot_mysql/todo_list/repository/TodoRepository.java
@@ -0,0 +1,26 @@
+package com.springboot_mysql.todo_list.repository;
+
+import java.util.ArrayList;
+
+public class TodoRepository {
+    ArrayList<ArrayList<String>> todos = new ArrayList<>();
+
+    public ArrayList<ArrayList<String>> getAllTodos(){
+        return (todos.isEmpty()) ? null : todos;
+    }
+
+    public ArrayList<String> getTodoForID(int id){
+        return (todos.isEmpty()) ? null : todos.get(id - 1);
+    }
+
+    public ArrayList<String> createTodo(String title, String description) {
+        ArrayList<String> req = new ArrayList<>();
+        req.add(title);
+        req.add(description);
+
+        System.out.println(req);
+
+        todos.add(req);
+        return req;
+    }
+}
